0e198a8fcc711b798a8796cbf21e1a7325b7a982
Link Clinton <LinkClinton@outlook.com>
2023-02-03 11:29:24 +0800

    remove all

--- path-tracing-gpu/extensions/glm/glm.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtx/matrix_decompose.hpp>
-#include <glm/gtc/quaternion.hpp>
-#include <glm/glm.hpp>
--- path-tracing-gpu/resources/shaders/depth_renderer/miss_shaders.hlsl
+++
@@ -1,8 +0,0 @@
-#include "depth_types.hlsl"
-
-[shader("miss")]
-void miss_shader(inout SYSTEM_VALUE(tracing_payload, payload, SV_RayPayload))
-{
-	payload.missed = 1;
-	payload.depth = 0;
-}
